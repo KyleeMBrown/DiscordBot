607645c7a6cff77a89c415cd940c1ce973387155
Kylee <kyleebrown.work@gmail.com>
2025-03-26 12:26:04 -0500

    removed old images added new

--- cogs/events/CharacterInfo.py
+++ cogs/events/CharacterInfo.py
@@ -8,23 +8,29 @@ with open('cogs/characterData.json', 'r') as file:
 
 # on server join
 class CharacterInfo(commands.Cog):  # Using commands.Cog even though you have only event listeners
+    # initialize bot
     def __init__(self, bot):
         self.bot = bot
 
-    # if user types [character] + info
+    # send character info after user prompts 
     @commands.Cog.listener()
     async def on_message(self, message):
+        # if bot is messaging do not respond
         if message.author == self.bot.user:
             return
+        # grab message content
         orig_str = message.content
+        # replace and lowercase the string to match data
         user_str = orig_str.replace("&", "and").lower()
         
-
+        # iterate through data
         for character in data:
+            #if user types [character] + info or a shortcut
             if (character["name"].lower() in user_str and "info" in user_str) or orig_str.lower() in character["shortcuts"]:
+                # character card
                 characterCard = discord.Embed(
                     title=character["name"],
-                    description=f'''
+                    description=f'''           
                     Role: {character['role']}\n
                     Pick Rate: {character['pickRate']}%
                     Win Rate: {character['winRate']}%
@@ -32,9 +38,11 @@ class CharacterInfo(commands.Cog):  # Using commands.Cog even though you have on
                     color=discord.Color.dark_gold()
                 )
                 characterCard.set_thumbnail(url=character['url'])
+                # stor author as user
                 user = message.author
+                # send info
                 await message.channel.send(f'Here is some info on **{character["name"]}** {user.mention}!' )
                 await message.channel.send(embed=characterCard )
-
+# cog setup
 async def setup(bot):
     await bot.add_cog(CharacterInfo(bot))     
