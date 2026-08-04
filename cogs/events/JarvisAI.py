c53fee80e6a9871424e5104a0ebdd1efed33acbf
Kylee <kyleebrown.work@gmail.com>
2026-08-04 13:20:08 -0500

    updated the model

--- cogs/events/JarvisAI.py
+++ cogs/events/JarvisAI.py
@@ -29,7 +29,7 @@ class JarvisAI(commands.Cog):  # Using commands.Cog even though you have only ev
         
         if orig_str.lower().startswith("jarvis"):
             response = client.models.generate_content(
-            model="gemini-2.0-flash", contents=f"{content} in 2000 characters or less"
+            model="gemini-2.0-flash-lite", contents=f"{content} in 2000 characters or less"
             )
             #print(response.text)
             #print(content)
